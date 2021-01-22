7ce0ce02b4505eb662f969330a6864f657aaa451
messiashu <claudineimessias@hotmail.com>
2021-01-22 17:40:41 -0300

    update

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -5,22 +5,33 @@ function Todo(){
 
     const [text, settext] = useState("");
 
+    const[items,setitems] = useState([])                 // criando um estado
+
     function adiciona(event){
         let te =event.target.value;
         settext(te);
         console.log(text)
     }
 
+    function adicionaItem(event){
+        event.preventDefault()
+        if(text){
+        setitems([...items,text])
+        settext("");
+        }
+    }
+
     return(<dir className="container">
         <h1>Todo</h1>
         <form>
-            <input onChange={adiciona} type="text"/>
-            <button>Add</button>
+            <input onChange={adiciona} type="text" value={text}/>
+            <button onClick={adicionaItem}>Add</button>
         </form>
          <ul>
-            <li>{text}</li>
+            {items.map(item=> <li>{item}</li>)}
          </ul>
-        </dir> )
+        </dir> 
+    )
 
 }
 
